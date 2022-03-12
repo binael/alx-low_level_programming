e23591664caaa78143ad51d1bd4fbc18a6f3f10e
binael <nbinael@yahoo.com>
2022-03-11 20:38:29 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - countdown of alphabets
+ * from a - z separated by newlint
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	for (n = 'a'; n = 'z'; n++)
+		putchar(n);
+		putchar('\n');
+		getch();
+
+	return (0);
+}
