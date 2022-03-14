8680ca00004e48b25d1372be05f992b80b70c186
binael <nbinael@yahoo.com>
2022-03-14 13:46:14 -0700

    combo advanced

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Print combed number format
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; ++i)
+	{
+		for (j = 0; j < 10; ++j)
+		{
+			if ((i - j == 1) || (i - j == 2) || (i - j == 3) || (i - j == 0))
+			{
+				continue;
+			}
+			else if ((i - j == 4) || (i - j == 5) || (i - j == 6))
+			{
+				continue;
+			}
+			else if ((i - j == 7) || (i - j == 8) || (i - j == 9))
+			{
+				continue;
+			}
+
+			putchar(i + '0');
+			putchar(j + '0');
+			putchar(44);
+			putchar(32);
+
+		}	
+	}
+
+	putchar(10);
+
+	return (0);
+}
