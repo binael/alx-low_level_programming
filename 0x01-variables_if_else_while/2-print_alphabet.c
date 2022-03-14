1d8161ad6f736760a111001e9a856321db421f3f
binael <nbinael@yahoo.com>
2022-03-14 01:40:15 -0700

    lower aphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
 
 /**
  * main - countdown of alphabets
@@ -10,14 +8,13 @@
 int main(void)
 {
 	int n;
-	int a = "/n";
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	
-	for (n = 'a'; n = 'z'; n++)
+	for (n = 'a'; n <= 'z'; n++)
 	{	putchar(n);
 	}
-	putchar(a);
+	putchar('\n');
 	return (0);
 }
