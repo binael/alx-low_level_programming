8b445cb6298bb539fa6f95247cd59da5718469d1
binael <nbinael@yahoo.com>
2022-03-14 09:40:46 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Print lowercase hexadecimal number
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; ++n)
+	{
+		putchar(n + '0');
+	}
+
+	for (n = 'a'; n <= 'f'; ++n)
+	{
+		putchar(n);
+	}
+
+	putchar(10);
+
+	return (0);
+}
