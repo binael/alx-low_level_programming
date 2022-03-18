2a451b1bd505ae398dbda3cd619dee4194f9a4db
binael <nbinael@yahoo.com>
2022-03-18 08:14:09 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main - Program that prints putchar
+ *
+ * Return: 0 (Success)
+ */
+
+void _putchar()
+void print_alphabet(void)
+
+int main(void)
+{
+	print_alphabet();
+
+	return (0);
+}
+
+void print_alphabet()
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar(10)
+}
