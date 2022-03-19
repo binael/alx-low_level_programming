8f82068487e98f5dc54539749f9905436e7107f3
binael <nbinael@yahoo.com>
2022-03-19 06:37:31 -0700

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the sign of numbers
+ * @n: Operand which can be negative or positive
+ *
+ * Return: n (number)
+ */
+int print_last_digit(int)
+{
+	return (n % 10);
+}
