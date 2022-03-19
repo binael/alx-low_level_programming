d3d614ba25bf7f79b5cf3c5a9764b0b632dd443b
binael <nbinael@yahoo.com>
2022-03-19 09:09:47 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the time
+ */
+void times_table(void)
+{
+	int i, j, k;
+	int unit, tens;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			k = i * j;
+			unit = k % 10;
+			ten = (k - (k % 10)) / 10;
+
+			if (k >= 10)
+				_putchar('0' + ten);
+			_putchar('0' + unit);
+
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(32);
+
+				if (k <= 9)
+					putchar(32);
+			}
+		}
+
+		putchar('\n');
+	}
+}
