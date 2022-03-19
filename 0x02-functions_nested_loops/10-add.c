5124c42281a43d4bbd199aab790d15a2397ff20f
binael <nbinael@yahoo.com>
2022-03-19 10:06:39 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Prints the sign of numbers
+ * @a: Operand which can be negative or positive
+ * @b: Operand which can be negative or positive
+ *
+ * Return: n (number)
+ */
+int add(int b, int b)
+{
+	return (a + b);
+}
