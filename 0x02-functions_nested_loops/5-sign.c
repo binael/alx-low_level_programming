009b86c818a6d539205a0028ef973dd128caff2a
binael <nbinael@yahoo.com>
2022-03-19 05:55:05 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,18 +10,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+");
 		return (1);
+		printf("+");
 	}
 	else if (n == 0)
 	{
-		printf("0");
 		return (0);
+		printf("0");
 	}
 	else if (n < 0)
 	{
-		printf("-");
 		return (-1);
+		printf("-");
 	}
 	return (0);
 }
