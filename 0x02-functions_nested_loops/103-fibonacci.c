10f9e93b44b8a38de4eb9ad114458abe0f2934c0
binael <nbinael@yahoo.com>
2022-03-20 05:56:29 -0700

    fibonacci add

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Function that prints Fibonacci numbers up to 50
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	double a, b, sum, even_sum;
+
+	a = 1;
+	b = 2;
+	sum = 0;
+	even_sum = 2;
+
+	while (sum <= 4000000)
+	{
+		sum = a + b;
+		a = b;
+		b = sum;
+
+		if ((sum % 2) == 0)
+			even_sum += sum;
+	}
+
+	printf("%.0f\n", even_sum);
+
+	return (0);
+}
