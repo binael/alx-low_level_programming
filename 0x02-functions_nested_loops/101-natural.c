9e7fee9ffbba32aec2b5147cbc796d9b0a93972b
binael <nbinael@yahoo.com>
2022-03-20 04:46:48 -0700

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -12,7 +12,7 @@ int main(void)
 
 	sum = 0;
 
-	for (i = 1; i < 1024)
+	for (i = 1; i < 1024; ++i)
 	{
 		if ((i % 3) == 0)
 			sum += i;
@@ -21,4 +21,6 @@ int main(void)
 	}
 
 	printf("%d\n", sum);
+
+	return (0);
 }
