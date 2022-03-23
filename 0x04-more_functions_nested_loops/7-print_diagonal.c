b62e9a8574e062c278599972ebfad8a41362e1b6
binael <99293451+binael@users.noreply.github.com>
2022-03-23 23:19:38 +0100

    Update 7-print_diagonal.c
    project

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,8 +1,11 @@
 #include "main.h"
 
 /**
- * print_diagonal - prints a diagonal as per number input
- * @n: number showing length of the diagonal
+ * print_diagonal - draws a diagonal line on the terminal
+ *
+ * @n: the number of times the character \ should be printed
+ *
+ * Return: void
  */
 void print_diagonal(int n)
 {
@@ -10,12 +13,9 @@ void print_diagonal(int n)
 
 	if (n > 0)
 	{
-		_putchar(92);
-		_putchar('\n');
-
-		for (i = 2; i <= n; i ++)
+		for (i = 0; i < n; i++)
 		{
-			for (j = 1; j < i; j++)
+			for (j = 0; j < l; j++)
 			{
 				_putchar(32);
 			}
