e537de6780f2df5c24235f044e06cad2025ceb90
binael <nbinael@yahoo.com>
2022-03-24 15:27:24 -0700

    str cat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strcat - Function that concatenates string
+ * @dest: destination
+ * @src: source
+ *
+ * Return: dest-value
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int len_src = 0;
+	int len_dest = 0;
+	int t = 1;
+
+	while (*(dest + len_dest) != '\0')
+		len_dest++;
+
+	while (t)
+	{
+		*(dest + len_dest) = *(src + len_src);
+
+		if (*(src + len_src) == '\0')
+			break;
+
+		len_src++;
+		len_dest++;
+	}
+
+	return (dest)
+}
