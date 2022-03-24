313696636ca1ba00c7891399e877f51adc54a748
binael <99293451+binael@users.noreply.github.com>
2022-03-25 00:21:36 +0100

    Create 5-string_toupper.c
    lower to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ *
+ * @s: string
+ *
+ * Return: dest
+ */
+ 
+char *string_toupper(char *s)
+{
+	int i;
+	char j;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		ch = *(s + i);
+
+		if (j >= 'a' && j <= 'z')
+			j += 'A' - 'a';
+
+		*(s + i) = j;
+	}
+
+	return (s);
+}
