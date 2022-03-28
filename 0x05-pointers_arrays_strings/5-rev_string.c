90f1bdf97dda98736622a3205dd4c397ba456605
binael <nbinael@yahoo.com>
2022-03-27 18:31:28 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses string
+ * @s: string to ber reversed
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int c1, c2;
+
+	int lenght = 0;
+	int boolean = 1;
+
+	while (boolean)
+	{
+		if (*(s + lenght) == '\0')
+		{
+			boolean = 0;
+		}
+
+		lenght += 1;
+	}
+
+	for (c1 = 0; c1 < lenght; ++c1)
+	{
+		_putchar(*(s + c1));
+	}
+
+	_putchar('\n');
+
+	for (c2 = (lenght - 1); c2 >= 0; --c2)
+	{
+		_putchar(*(s + c2));
+	}
+
+	_putchar('\n');
+}
