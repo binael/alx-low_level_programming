15e2c3eea3638eac83224280dc8d8f60b565a64b
binael <nbinael@yahoo.com>
2022-03-28 16:03:06 -0700

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,9 +12,13 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (*(src + i) <= '\0')
+	while (i >= 0)
 	{
 		*(dest + i) = *(src + i);
+
+		if ((*src + i) == '\0')
+			break;
+
 		i += 1;
 	}
 
