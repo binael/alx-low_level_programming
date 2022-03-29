e197c631aced5d50faaf4ddf95b05b636b8a398c
binael <nbinael@yahoo.com>
2022-03-29 07:41:46 -0700

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,24 +10,15 @@
 int _atoi(char *s)
 {
 	int num = 0;
-	int check = 1;
 	int i = 0;
 	int sign = 1;
 
 	while (*(s + i) != '\0')
 	{
-		if (check == 1)
-		{	if (*(s + i) == '-')
-			{
-				sign = -1;
-				check = 0;
-			}
-		}
+		if (*(s + i) == '-')
+			sign *= -1;
 		if ((*(s + i) >= '0') && (*(s + i) <= '9'))
-		{
-			num = num * 10 + (*(s + i) + '0');
-			check = 0;
-		}
+			num = num * 10 + (*(s + i) - '0');
 		if ((num > 0) && (*(s + i) < '0' || *(s + i) > '9'))
 			break;
 
