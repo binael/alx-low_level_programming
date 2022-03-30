1883a10bb245c6d13616a290d505e335136c4a35
binael <nbinael@yahoo.com>
2022-03-30 15:33:08 -0700

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the power of an integer
+ * @x: the integer base or number
+ * @y: the integer power
+ *
+ * Return: x power y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	else if (y < 0)
+		return (-1);
+
+	return (x * _pow_recursion(x, y - 1));
+}
