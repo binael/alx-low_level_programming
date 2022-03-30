d5ae56487ea3bf4e208e3eb0c743a7a360d28f7d
binael <nbinael@yahoo.com>
2022-03-30 14:48:27 -0700

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints reverse of string
+ * @s: string input
+ *
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (s == '\0')
+		return;
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
