612922049fd5e4b88c024d240d9d5079723d511a
binael <nbinael@yahoo.com>
2022-04-01 11:39:45 -0700

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds all digits in the command line
+ * @argc: command line number of inputs
+ * @argv: command line inputs
+ *
+ * Return: 0 (success), 1 (failure)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum;
+	
+	sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		while (*argv[i] != '\0')
+		{
+			if ((*argv[i] < '0') || (*argv[i] > '9'))
+			{
+				printf("Error\n");
+				return (1);
+			}
+
+			argv[i]++;
+		}
+
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
