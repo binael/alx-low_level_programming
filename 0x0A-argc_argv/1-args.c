f857bed5f51b34fb1742d83fff57b3116a94ccd7
binael <nbinael@yahoo.com>
2022-04-01 07:09:06 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of command line inputs
+ * @argc: integer value of number of command line inputs
+ * @argv: characters for each command line input
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char __attribute__((___unused__)) *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
