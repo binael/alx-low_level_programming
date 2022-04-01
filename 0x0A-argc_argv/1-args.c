4947f39529786e1544340d55c984e97890e64daf
binael <nbinael@yahoo.com>
2022-04-01 07:16:16 -0700

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,7 +8,7 @@
  * Return: Always 0 (success)
  */
 
-int main(int argc, char __attribute__((___unused__)) *argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
 	printf("%d\n", argc - 1);
 
