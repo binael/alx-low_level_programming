927a90b81875fb1ded6c2638283bd8f4cf9c6059
binael <nbinael@yahoo.com>
2022-04-01 05:54:52 -0700

    README

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - print the name of the program
+ * @argc: the number of command line inputs
+ * @argv: the name of the command line string input
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv)
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
