66a383669d583c6bf53599457341c54b3705ec26
binael <nbinael@yahoo.com>
2022-04-01 07:55:05 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints the command line arguments
+ * @argc: integer number of arguments
+ * @argv: character-command line string inputs
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	(void) argc;
+	(void) argv;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argc[i]);
+	}
+
+	return (0);
+}
