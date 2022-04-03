fad2a5db239d6ae3ee8b9b93854dfb331a6394a3
binael <nbinael@yahoo.com>
2022-04-03 15:30:55 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - keygen for crackme
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+	int num;
+
+	srand(time(0));
+	num = rand();
+	printf("%i\n", num);
+
+	return (0);
+}
