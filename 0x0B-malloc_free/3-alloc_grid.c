7f2f32ab734b74e3886dda02cef2a261c4054b21
binael <nbinael@yahoo.com>
2022-04-05 10:25:39 -0700

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,7 +18,7 @@ int **alloc_grid(int width, int height)
 	if ((width <= 0) || (height <= 0))
 		return (NULL);
 
-	array = malloc(heigth * sizeof(int *));
+	array = malloc(height * sizeof(int *));
 	if (array == NULL)
 		return (NULL);
 
