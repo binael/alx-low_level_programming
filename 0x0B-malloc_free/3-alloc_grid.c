dfd35f8c518e0fdf3789b67958813bd5a0c5bebb
binael <nbinael@yahoo.com>
2022-04-05 14:03:30 -0700

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,7 +26,13 @@ int **alloc_grid(int width, int height)
 	{
 		array[i] = malloc(width * sizeof(int));
 		if (array[i] == NULL)
+		{
+			for (i = i - 1; i >= 0; i--)
+				free array[i];
+
+			free array;
 			return (NULL);
+		}
 	}
 
 	for (i = 0; i < height; i++)
