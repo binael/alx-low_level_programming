f8ef0e18024e8680b5950611e611678836ad2515
binael <nbinael@yahoo.com>
2022-04-05 09:16:14 -0700

    task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int size;
+	char *new_string;
+
+	if (s1 == NULL)
+		s1[0] = "";
+
+	if (s2 == NULL)
+		s2[0] = "";
+
+	size = strlen(s1) + strlen(s2) + 1;
+	new_string = malloc(size * sizeof(char));
+
+	if (new_string == NULL)
+		return (NULL);
+
+	strcpy(new_string, s1);
+	strcat(new_string, s2);
+
+	return (new_string);
+}
