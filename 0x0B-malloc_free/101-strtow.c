3bd4ddf14ab7f9c8ef252728a3d31ebcd5e273eb
binael <nbinael@yahoo.com>
2022-04-05 17:49:55 -0700

    task 6

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * strtow - splits a string into two words
+ * @str: the string input
+ *
+ * Return: array of two words
+ */
+
+char **strtow(char *str)
+{
+	char **word;
+	int len;
+
+	int i = 0, row = 0, col = 0, len1 = 0;
+
+	if ((str == NULL) || (str == ""))
+		return (NULL);
+
+	word = malloc(sizeof(char *) * 3);
+	if (word == NULL)
+		return (NULL);
+
+	while (*(str + len1) != ' ')
+		len1 += 1;
+
+	len = strlen(str);
+	word[0] = malloc((len1 + 2) * sizeof(char));
+	word[1] = malloc((len - len1 + 1) * sizeof(char));
+	word[2] = malloc(1 * sizeof(char));
+
+	if ((word[0] == NULL) || (word[1] == NULL) || (word[2] == NULL))
+		return (NULL);
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == ' ')
+		{
+			word[col][row] == '\0';
+			col = 1;
+			row = 0;
+			continue;
+		}
+
+		word[col][row] = *(s + i);
+		i += 1;
+		row += 1;
+	}
+
+	word[col][row] = '\0';
+	word[2][0] = '\0';
+
+	return (word);
+}
