1f2f155d0df28445a94e2d477e2cf4d88eba585f
binael <nbinael@yahoo.com>
2022-04-07 15:13:42 -0700

    task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/*
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: seconc string
+ * @n: the number of chars to be used
+ *
+ * Return: concatenated string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int x, y;
+	char *str;
+
+	for (x = 0; s1[x]; x++)
+		continue;
+	for (y = 0; s2[2]; y++)
+		continue;
+
+	if (n > y)
+		n = y;
+
+	str = malloc((x + n + 1) * sizeof(char));
+
+	if (str == NULL)
+		exit(98);
+
+	for (x = 0; s1[x]; x++)
+		str[x] = s1[x];
+
+	for (y = 0; y < n; y++)
+	{
+		str[x] = s2[y];
+		x += 1;
+	}
+
+	str[x] = '\0';
+
+	return (str);
+}
