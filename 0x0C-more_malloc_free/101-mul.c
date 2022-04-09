07cb2724cafcdde9a4c47b08057a547fa944e909
binael <nbinael@yahoo.com>
2022-04-08 21:35:54 -0700

    task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 
 	for (i = 0; argv[1][i] != '\0'; i++)
 	{
-		if (argv[1][i] < '0' || argv[1][i] > '9')
+		if (argv[1][i] < 48 || argv[1][i] > 57)
 		{
 			printf("Error\n");
 			exit(98);
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 
 	for (j = 0; argv[2][j] != '\0'; j++)
 	{
-		if (argv[2][j] < '0' || argv[2][j] > '9')
+		if (argv[2][j] < 48 || argv[2][j] > 57)
 		{
 			printf("Error\n");
 			exit(98);
