ef61c2a06cdf45f3937032a9e89c579a97185531
binael <nbinael@yahoo.com>
2022-04-08 21:21:00 -0700

    task 5

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: integer number of command line arguments
+ * @argv: command line arguments array
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	unsigned long mul;
+	int i, j;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	for (i = 0; argv[1][i] != '\0'; i++)
+	{
+		if (argv[1][i] < '0' || argv[1][i] > '9')
+		{
+			printf("Error\n");
+			exit (98);
+		}
+	}
+
+	for (j = 0; argv[2][j] != '\0'; j++)
+	{
+		if (argv[2][i] < '0' || argv[2][i] > '9')
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
+
+	mul = atoi(argv[1]) * atoi(argv[2]);
+
+	printf("%lu\n", mul);
+
+	return (0);
+}
