907477eae5f9f4f6e7acd44293ce4bbf3a3d26b1
binael <nbinael@yahoo.com>
2022-04-08 17:56:54 -0700

    task 4

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _realloc - reallocates memory
+ * @ptr: void pointer
+ * @old_size: int initial pointer size
+ * @new_size: new size
+ *
+ * Return: void pointer
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *new_ptr, *copy;
+	unsigned int i;
+
+	if (ptr != NULL)
+		copy = ptr;
+	else
+		return (malloc(new_size));
+
+	if (old_size == new_size)
+		return (ptr);
+
+	if ((ptr != NULL) && (new_size == 0))
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	new_ptr = malloc(new_size);
+
+	if (new_ptr == NULL)
+		return (NULL);
+
+	for (i = 0; (i < old_size || i < new_size); i++)
+		*(new_ptr + 1) = copy[i];
+
+	free(ptr);
+
+	return (new_ptr);
+}
