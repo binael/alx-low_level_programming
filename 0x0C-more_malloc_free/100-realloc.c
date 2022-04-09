0ab7aa8741d74cfdd4c94bd1a3d1a770ed3a2706
binael <nbinael@yahoo.com>
2022-04-08 20:39:12 -0700

    task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -25,13 +25,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if ((ptr != NULL) && (new_size == 0))
 	{
 		free(ptr);
-		return (NULL);
+		return (0);
 	}
 
 	new_ptr = malloc(new_size);
 
 	if (new_ptr == NULL)
-		return (NULL);
+		return (0);
 
 	for (i = 0; (i < old_size || i < new_size); i++)
 		*(new_ptr + 1) = copy[i];
