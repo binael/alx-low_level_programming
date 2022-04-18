8747084ed243320b9a339cb360092528b57e2c67
binael <99293451+binael@users.noreply.github.com>
2022-04-18 14:49:25 +0100

    Update 5-free_dog.c
    finale

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,1 +1,17 @@
- fkafkafiiaffafk di
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees a dog
+ *
+ * @d: struct dog
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
