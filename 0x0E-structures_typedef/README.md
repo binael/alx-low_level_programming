5104c2ccce66cfd471f8aaa776ba1f0921bf05d2
binael <nbinael@yahoo.com>
2022-04-18 06:02:16 -0700

    main.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,4 @@
+_*0x0E. C - Structures, typedef*_
+
+- _*What are structures, when, why and how to use them*_
+- _*How to use "typedef"*_
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+struct dog
+{
+	char *name;
+	floar age;
+	char *owner;
+};
+
+#endif
