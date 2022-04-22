aecc435b7fac44e90d67eef1fa29be9f0a8d6868
binael <nbinael@yahoo.com>
2022-04-22 06:44:17 -0700

    array compare

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - search for an integer
+ * @array: given array
+ * @size: int array size
+ * @cmp: function pointer which is the comparator
+ *
+ * Return: returns the first element
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i] != 0))
+			return (i);
+	}
+
+	return (-1);
+}
+
