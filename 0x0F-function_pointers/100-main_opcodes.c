4a267da887d6d9ef9bab62a70b61efac0b0f9c69
binael <nbinael@yahoo.com>
2022-04-24 03:28:15 -0700

    opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main opcode printing
+ * @argc: integer number of arguments
+ * @argv: array of command line arguments
+ *
+ * Return: exit status
+ */
+
+int main(int argc, char **argv)
+{
+	int num;
+	char *m;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit (1);
+	}
+
+	num = atoi(argv[1]);
+
+	if (argv < 0)
+	{
+		printf("Error\n");
+		exit (2);
+	}
+
+	m = (char *)main;
+
+	printf("%02x", m[0]);
+
+	for (i = 1; i < argc; i++)
+	{
+		printf(" %02x", m[i]);
+	}
+
+	printf("\n");
+
+	return (0);
+}
