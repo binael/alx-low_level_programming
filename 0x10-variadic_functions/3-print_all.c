a0bf8177a705e4fedeadbbaaec6936b60245594a
binael <nbinael@yahoo.com>
2022-04-25 20:51:27 -0700

    print all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,49 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - print character, float, string and integer
+ * @format: constant string format
+ *
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	char *s;
+	int i = 0, b = 1;
+
+	va_start(ap, format);
+
+	while (*(format + i))
+	{
+		switch (*(format + i))
+		{
+			case 'c':
+				printf("%c", va_arg(ap, int));
+				break;
+			case 'i':
+				printf("%i", va_arg(ap, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(ap, double));
+				break;
+			case 's':
+				s = va_arg(ap, char *);
+				if (s == NULL)
+					printf("%p", s);
+				else
+					printf("%s", s);
+				break;
+			default:
+				b = 0;
+		}
+		if (*(format + i + 1) && b)
+			printf(", ");
+
+		b = 1;
+		i++;
+	}
+	printf("\n");
+	va_end(ap);
+}
