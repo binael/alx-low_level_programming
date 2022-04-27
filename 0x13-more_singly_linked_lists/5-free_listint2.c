646f609ab1f105e777204eb9772536a13b300863
binael <99293451+binael@users.noreply.github.com>
2022-04-27 04:44:27 +0100

    updated free list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,1 +1,24 @@
-yfugufthgtxtbh
+#include "lists.h"
+
+/**
+ * free_listint2 - free a linked list
+ * @head: list head
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+	listint_t *my_list;
+
+	if (head)
+	{
+		curr = *head;
+		while ((temp = my_list) != NULL)
+		{
+			my_list = my_list->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
