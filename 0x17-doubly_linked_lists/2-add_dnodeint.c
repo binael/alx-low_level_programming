4232547342d93a3e5c342b5cf14eeb12a798d644
binael <nbinael@yahoo.com>
2022-06-09 15:06:29 -0700

    message

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - add a node
+ * @head: list head
+ * @n: const integer
+ *
+ * Return: address of the new assignment
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t new, d_list;
+
+	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+	{
+		printf("Malloc failed");
+		return NULL;
+	}
+
+	new->n = n;
+	d_list = *head;
+	new->prev = NULL;
+	new->next = d_list;
+
+	if (d_list)
+	{
+		d_list->prev = new;
+	}
+
+	d_list = new;
+
+	return (d_list);
+}
