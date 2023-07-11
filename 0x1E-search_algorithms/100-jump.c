a6892a3c8f23d284ee9de883762ba942e6e00293
binael <nbinael@yahoo.com>
2023-07-11 14:55:30 +0100

    accounted for values less than first array value

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -13,9 +13,7 @@ int jump_search(int *array, size_t size, int value)
 	size_t root, max, min;
 
 	if (array == NULL)
-	{
 		return (-1);
-	}
 
 	root =  (size_t)sqrt(size);
 	max = min = 0;
@@ -25,26 +23,29 @@ int jump_search(int *array, size_t size, int value)
 		printf("Value checked array[%ld] = [%d]\n", max, array[max]);
 		if (array[max] >= value || max >= size)
 		{
+			if (max == 0 && array[max] > value)
+				return (-1);
+			else if (max == 0)
+				return (max);
+
 			min = max - root;
 			printf("Value found between indexes [%ld] and [%ld]\n", min, max);
 			break;
 		}
 		max = max + root;
 	}
-
 	min = max - root;
 
 	if (max >= size)
-	{
 		max = size - 1;
-	}
+
 	for (min = min; min <= max; min++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", min, array[min]);
+
 		if (array[min] == value)
-		{
 			return (min);
-		}
+
 	}
 	return (-1);
 }
