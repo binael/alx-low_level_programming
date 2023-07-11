875a76fc6e0aa73e3099acdc4b4869d07fc48e1f
binael <nbinael@yahoo.com>
2023-07-11 14:43:33 +0100

    implentation of jump search algorithm

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,50 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - Implements jump search algorithm to find a target
+ * @array: the array to search
+ * @size: the size of the array (length)
+ * @value: the target value to search for
+ *
+ * Return: the index of the target
+*/
+int jump_search(int *array, size_t size, int value)
+{
+	size_t root, max, min;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	root =  (size_t)sqrt(size);
+	max = min = 0;
+
+	while (root)
+	{
+		printf("Value checked array[%ld] = [%d]\n", max, array[max]);
+		if (array[max] >= value || max >= size)
+		{
+			min = max - root;
+			printf("Value found between indexes [%ld] and [%ld]\n", min, max);
+			break;
+		}
+		max = max + root;
+	}
+
+	min = max - root;
+
+	if (max >= size)
+	{
+		max = size - 1;
+	}
+	for (min = min; min <= max; min++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", min, array[min]);
+		if (array[min] == value)
+		{
+			return (min);
+		}
+	}
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,9 +4,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_search_zone(int *array, size_t low, size_t high);
+int jump_search(int *array, size_t size, int value);
 
 #endif /*end of include*/
